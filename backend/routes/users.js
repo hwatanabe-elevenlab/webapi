ea557a9f0ef0ef014bd527f50c48d6aa66de1461
hwatanabe-elevenlab <hwatanabe.elevenlab@gmail.com>
2021-03-11 17:11:40 +0900

    b

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,9 +5,10 @@ var fetch = require('node-fetch');
 
 /* GET users listing. */
 router.get('/', cors(), async function(req, res, next) {
-  const response = await fetch('http://192.168.100.14:30000/samples/users');
-  const data = await response.json();
-  const users = data;
+  // const response = await fetch('http://192.168.100.14:30000/samples/users');
+  // const data = await response.json();
+  // const users = data;
+  const users = [{id: 1, name: 'test'}]
   res.send(users);
 });
 
